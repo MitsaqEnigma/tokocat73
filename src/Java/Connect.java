8aedf864a859f1004baa4cfbce3e2fa5c146ff17
Mitsaq Enigma al Afghan <enigma.afghan@gmail.com>
2018-04-05 13:29:57 +0700

    Initial commit

---
+++ src/Java/Connect.java
@@ -0,0 +1,63 @@
+package Java;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+public class Connect {
+    
+    public Connection conn;
+    public Statement st;
+
+    public Connect() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/k9170490_tokowp", "root", "");
+            st = conn.createStatement();
+            System.out.println("Connection Successful!");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.err.println(e.toString());
+            System.exit(1);
+        }
+    }
+    
+    public Connection Connect(){
+        return conn;
+    }
+    
+    public void simpanData(String sql) {
+        try {
+            Connect();
+            st.executeUpdate(sql);
+        } catch (SQLException x) {
+            JOptionPane.showMessageDialog(null, "Simpan Data Gagal, Pesan error : \n" + x);
+        }
+    }
+
+    public void tutupKoneksi(ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+            st.close();
+            conn.close();
+        } catch (SQLException x) {
+            JOptionPane.showMessageDialog(null, "Tutup Koneksi Gagal, Pesan error \n" + x);
+        }
+    }
+
+    public ResultSet ambilData(String sql) {
+        ResultSet rs = null;
+        try {
+            Connect();
+            rs = st.executeQuery(sql);
+        } catch (SQLException x) {
+            JOptionPane.showMessageDialog(null, "Ambil Data Gagal, Pesan error : \n" + x);
+        }
+        return rs;
+    }
+    
+}
