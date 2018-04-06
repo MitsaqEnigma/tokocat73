b3b915d27746cc8da44b8cd72ef27f9548f76e42
Windychua <whitelovers12@gmail.com>
2018-04-06 16:49:28 +0700

    laporan toko

---
+++ src/Java/ListTokoLaporanToko.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Java;
+
+/**
+ *
+ * @author Miracle
+ */
+public class ListTokoLaporanToko {
+    private String no_faktur, tanggal;
+    private int total, saldo;
+
+    public String getNo_faktur() {
+        return no_faktur;
+    }
+
+    public void setNo_faktur(String no_faktur) {
+        this.no_faktur = no_faktur;
+    }
+
+    public String getTanggal() {
+        return tanggal;
+    }
+
+    public void setTanggal(String tanggal) {
+        this.tanggal = tanggal;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+    
+    
+}
